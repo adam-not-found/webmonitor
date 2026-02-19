6a7479b457f1921d8baf2e2c71180e4ff9b547ae
Adam Erickson <adam.erickson@my.wheaton.edu>
2026-02-19 13:04:17 -0600

    Updated alert defaults and added mandatory security notifications

--- webmonitor.sh
+++ webmonitor.sh
@@ -5,15 +5,22 @@ CONFIG="$HOME/.webmonitor/config.json"
 get_val() { python3 -c "import json,os; print(json.load(open('$CONFIG'))['$1'])" 2>/dev/null; }
 save_val() { python3 -c "import json; d=json.load(open('$CONFIG')); d['$1']=$2; json.dump(d, open('$CONFIG', 'w'), indent=4)" ; }
 
-# Alert Toggling Logic
 manage_alerts() {
     while true; do
         echo -e "\n${BLUE}--- MANAGE ALERTS (Toggle On/Off) ---${NC}"
-        python3 -c "import json; d=json.load(open('$CONFIG')); [print(f'{i+1}) [{\"ON\" if v else \"OFF\"}] {k}') for i, (k, v) in enumerate(d['alerts'].items())]"
+        # Settings Adjusted is mandatory - we won't show it in the list to prevent tampering
+        python3 -c "import json; d=json.load(open('$CONFIG')); [print(f'{i+1}) [{\"ON\" if v else \"OFF\"}] {k}') for i, (k, v) in enumerate(d['alerts'].items()) if k != 'settings_adjusted']"
         echo "0) Back to Main Menu"
         read -p "Select a number to toggle: " alert_num
         [[ "$alert_num" == "0" ]] && break
-        python3 -c "import json; d=json.load(open('$CONFIG')); keys=list(d['alerts'].keys()); k=keys[$alert_num-1]; d['alerts'][k]=not d['alerts'][k]; json.dump(d, open('$CONFIG', 'w'), indent=4)"
+        
+        # Toggle the value and send the mandatory notification
+        key=$(python3 -c "import json; d=json.load(open('$CONFIG')); keys=[k for k in d['alerts'].keys() if k != 'settings_adjusted']; print(keys[$alert_num-1])")
+        python3 -c "import json; d=json.load(open('$CONFIG')); k='$key'; d['alerts'][k]=not d['alerts'][k]; json.dump(d, open('$CONFIG', 'w'), indent=4)"
+        
+        # Mandatory notification that settings were changed
+        python3 $HOME/webmonitor/monitor.py --alert "settings_adjusted" "Alert toggle changed: $key" &
+        echo -e "${GREEN}Toggle updated. Mandatory notification sent.${NC}"
     done
 }
 
@@ -27,7 +34,6 @@ manage_list() {
             1) python3 -c "import json; d=json.load(open('$CONFIG')); items=sorted(d['$key']); [print(f' • {i}') for i in items] if items else print('Empty')" ;;
             2) read -p "Enter $name: " val
                python3 -c "import json; d=json.load(open('$CONFIG')); d['$key'].append('$val'); json.dump(d, open('$CONFIG', 'w'), indent=4)"
-               # TRIGGER ALERT LOGIC (Add)
                python3 $HOME/webmonitor/monitor.py --alert "added_$key" "$val" & ;;
             3) items=($(python3 -c "import json; d=json.load(open('$CONFIG')); print(' '.join(sorted(d['$key'])))"))
                for i in "${!items[@]}"; do echo "$((i+1))) ${items[$i]}"; done
@@ -59,6 +65,7 @@ while true; do
             read -p "Change? (1-4): " c_opt
             read -p "New Value: " n_val
             if [[ "$c_opt" == "3" ]]; then
+                # Send to OLD recipient first
                 python3 $HOME/webmonitor/monitor.py --alert "recipient_changed" "$n_val" "$old_recipient"
             fi
             case $c_opt in
@@ -66,7 +73,8 @@ while true; do
                 2) save_val "app_password" "'$(echo $n_val | tr -d ' ')'" ;;
                 3) save_val "recipient_email" "'$n_val'" ;;
                 4) [[ "$n_val" =~ ^[Yy]$ ]] && save_val "cc_email" "'$(get_val sender_email)'" || save_val "cc_email" "''" ;;
-            esac ;;
+            esac
+            python3 $HOME/webmonitor/monitor.py --alert "settings_adjusted" "Account details modified" & ;;
         2) manage_list "trigger_words" "Trigger Word" ;;
         3) manage_list "whitelist" "Whitelisted Site" ;;
         4) manage_alerts ;;
